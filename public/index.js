9bf08d405642d5f690b1aa6c0181f7a536c5df24
Ying <86930915+Yl9464@users.noreply.github.com>
2026-05-14 18:33:31 -0400

    score tracked, header updates with winner name or draw, alert for reset game

--- public/index.js
+++ public/index.js
@@ -1,4 +1,6 @@
 let totalScore = 0;
+let score1 = 0;
+let score2 = 0;
 
 function player1(dice1) {
   document
@@ -13,39 +15,43 @@ function player2(dice2) {
 }
 
 function rollAgain() {
-
-  // Roll new dice values
   let dice1 = Math.floor(Math.random() * 6) + 1;
   let dice2 = Math.floor(Math.random() * 6) + 1;
 
-  // Update dice images
-  player1(dice1);
-  player2(dice2);
+  document.querySelector(".img1").setAttribute("src", "images/dice" + dice1 + ".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice" + dice2 + ".png");
 
-  // Determine winner
   if (dice1 > dice2) {
-      document.getElementById("score1").innerHTML = totalScore;
-     totalScore += 1;
-
-  } else if (dice1 < dice2) {
-      document.getElementById("score2").innerHTML = totalScore;
-    totalScore += 1;
-  } else {
-    document.getElementById("score1").innerHTML = totalScore;
-    document.getElementById("score2").innerHTML = totalScore;
-
-    totalScore += 1;
+     score1++;
+    document.getElementById("score1").innerHTML = score1;
+  } 
+  else if (dice2 > dice1) {
+    score2++;
+    document.getElementById("score2").innerHTML = score2;
+  } 
+  else {
+   score1++;
+    document.getElementById("score1").innerHTML = score1;
+    score2++;
+    document.getElementById("score2").innerHTML = score2;
 
   }
+document.querySelector(".header").innerHTML =
+    dice1 > dice2 ? "Player 1 Wins!" :
+    dice2 > dice1 ? "Player 2 Wins!" :
+    "Draw!";
 }
-
 function newGame() {
+  score1 = 0;
+  score2 = 0;
+
   document.getElementById("score1").innerHTML = 0;
   document.getElementById("score2").innerHTML = 0;
 
   document.querySelector(".img1").setAttribute("src", "images/dice1.png");
   document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-  
-  confirm("Game Restarted")
 
+  document.querySelector(".header").innerHTML = "Dice Game";
+ 
+  confirm("Game Restarted")
 }
